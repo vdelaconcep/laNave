38eb10dadfd3fc5e66d1a298326ebf106d8b5e3b
vdelaconcep <vdelaconcep@gmail.com>
2025-07-10 17:48:15 -0300

    Lista sección 'Nosotros'

--- front/src/pages/nosotros.jsx
+++ front/src/pages/nosotros.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useContext } from 'react';
 import { BackgroundContext } from '@/context/backgroundContext';
+import nosotros from '@/assets/img/nosotros.jpg';
+import '@/pages/css/nosotros.css'
 
 const Nosotros = () => {
     const { setBackground } = useContext(BackgroundContext);
@@ -11,7 +13,19 @@ const Nosotros = () => {
 
     return (
         <main>
-            <h1 className="pagina-titulo text-white text-center">Nosotros</h1>
+            <h1 className="pagina-titulo text-white text-center">Sobre nosotros</h1>
+            <section className='nosotros-section container mt-4 mb-5 d-flex flex-column align-items-center'>
+                <p>La Nave es un espacio que celebra nuestra identidad. Nació del amor por los artistas que hicieron del rock nacional una forma de vida, y que siguen marcando generaciones con su música.
+                <br /><br />
+                    Desde acá queremos rendir homenaje a quienes, entre poesía y guitarras, nos enseñaron a sentir orgullo por lo que somos. Porque el rock argentino nos representa.
+                </p>
+                <div className='d-flex justify-content-center mb-4 mt-2'>
+                    <div className='nosotros-fotoDiv'>
+                        <img className='w-100' src={nosotros} alt="Nosotros en Rumipal La Renga 2014" />
+                    </div>
+                </div>
+                <h1 className='text-center'>¡El Rock Nacional es orgullo e identidad!</h1>
+            </section>
         </main>
     );
 };
