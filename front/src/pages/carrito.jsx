b4ffb4f21a15f819d2f289867cd0131b60e6cd3c
Valeria <vdelaconcep@gmail.com>
2025-07-29 21:19:31 -0300

    Trabajando en mensaje de carrito vacío

--- front/src/pages/carrito.jsx
+++ front/src/pages/carrito.jsx
@@ -1,29 +1,44 @@
-import { useEffect, useContext } from 'react';
+import { useEffect, useContext, useState } from 'react';
 import { BackgroundContext } from '@/context/backgroundContext';
 import { Link } from 'react-router-dom';
-import carritoVacio from '@/assets/img/carritoVacio.jpg';
+import { CarritoContext } from '@/context/carritoContext';
+import carritoVacioImagen from '@/assets/img/carritoVacio.jpg';
 import '@/pages/css/carrito.css'
 
 
 const Carrito = () => {
     const { setBackground } = useContext(BackgroundContext);
     
-        useEffect(() => {
-            setBackground('bg-carrito');
-            return () => setBackground('');
-        }, []);
+    useEffect(() => {
+        setBackground('bg-carrito');
+        return () => setBackground('');
+    }, []);
+    
+    const { carrito, setCarrito } = useContext(CarritoContext);
+
+    const [carritoVacio, setCarritoVacio] = useState(false);
+
+    useEffect(() => {
+        const estaVacio = Array.isArray(carrito) && carrito.length === 0;
+        if (estaVacio) setCarritoVacio(estaVacio);
+    }, [carrito])
     
     return (
         <main>
             <h1 className="pagina-titulo text-white text-center">Carrito</h1>
             <section className='carritoVacio-section aparecer container mt-2 mb-5 d-flex flex-column align-items-center'>
-                <h5 className='text-center'>Todavía no hay ítems en tu carrito</h5>
-                <div className='d-flex justify-content-center mb-4 mt-2'>
-                    <div className='carritoVacio-fotoDiv'>
-                        <img className='w-100' src={carritoVacio} alt="Intoxicados en Tilcara" />
+                {carritoVacio &&
+                    <>
+                    <h5 className='text-center'>Todavía no hay ítems en tu carrito</h5>
+                    <div className='d-flex justify-content-center mb-4 mt-2'>
+                        <div className='carritoVacio-fotoDiv'>
+                            <img className='w-100' src={carritoVacioImagen} alt="Intoxicados en Tilcara" />
+                        </div>
                     </div>
-                </div>
-                <h5 className='text-center'>¡Te esperamos en la sección <Link to='/productos' className='carritoVacio-link'>productos</Link>!</h5>
+                    <h5 className='text-center'>¡Te esperamos en la sección <Link to='/productos' className='carritoVacio-link'>productos</Link>!</h5>
+                </>
+                }
+                
             </section>
         </main>
     );
